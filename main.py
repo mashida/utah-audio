1db41045eeb98ff17f37897a2999e4c2b956ab6f
Nikita Serditov <34740100+mashida@users.noreply.github.com>
2025-05-09 09:00:24 +0300

    added click usage

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+import click
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
 
-if __name__ == '__main__':
-    url = "https://www.youtube.com/watch?v=jqS4S_jxLdo"
-
+@click.command()
+@click.argument('url')
+def download_video_by_link(url: str) -> None:
+    """Download Youtube video via url"""
     yt = YouTube(url, on_progress_callback=on_progress)
     print(yt.title)
 
     ys = yt.streams.get_audio_only()
     ys.download()
+
+if __name__ == '__main__':
+    download_video_by_link()
